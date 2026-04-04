44e895b8bf7b4c37aa0065b81d6d9d55b5a208d2
Inna <ogsingerlan13@gmail.com>
2026-04-04 17:58:30 +0300

    Update circle.h

--- Point/circle.h
+++ Point/circle.h
@@ -1,11 +1,14 @@
+#pragma once
+
 #include "point.h"
 
 class Circle:public Point{
 
-    protected:
-    double r;
+    //protected:
+    //double r;
 
     public:
+    double r;
     Circle(double xi, double yi, double ri):Point(xi, yi){ r = ri;}
     double area();
 };
