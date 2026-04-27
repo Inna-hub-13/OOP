07ce766b88ab711b5d0798c9dd7258d073b19708
Inna <ogsingerlan13@gmail.com>
2026-04-27 11:33:30 +0300

    Update main.cpp

--- lab8/main.cpp
+++ lab8/main.cpp
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <string.h>
-#include "DesTable.h"
+#include "DihotTable.h"
 
 FILE* fp;
 
@@ -12,16 +12,21 @@ int main() {
 
     fopen_s(&fp, "des.txt", "r");
 
-    DesTable table = DesTable();
+    DihotTable table = DihotTable();
 
     do{
 
         fscanf(fp, "%s %s", iden, typ);
         *(typ  + strlen(typ) - 1) = '\0';
         l1 = LexUnit(iden, typ);
-        table.AddAtEnd(&l1);
+        // Invalid element not added to table
+        if(l1.getAddress() != 10000000)
+            table.Add(&l1);
     }while(!feof(fp));
 
+    table.print();
+    table.Delete("b");
+    table.Delete("e");
     table.print();
 
     fclose(fp);
