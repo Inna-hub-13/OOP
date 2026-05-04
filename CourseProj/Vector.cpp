13a3a61f95f79f4766f8935dc35ba70bbaa1a801
Inna <ogsingerlan13@gmail.com>
2026-05-04 18:21:13 +0300

    Update Vector.cpp

--- CourseProj/Vector.cpp
+++ CourseProj/Vector.cpp
@@ -10,16 +10,32 @@ template<typename T> Vector<T>::Vector(){
     _size = 0;
 }
 
-template<typename T> Vector<T>::Vector(Vector& copy_vector){
+template<typename T> Vector<T>::Vector(const Vector& copy_vector){
 
     _size = _capacity = copy_vector.size();
     vector = new T[_capacity];
 
     unsigned i = 0;
-    for(auto el : copy_vector){
-        
-        *(vector + i) = el;
-        i++;
+    for(;i < copy_vector._size; i++)
+        *(vector + i) = copy_vector.vector[i];
+}
+
+template<typename T> Vector<T>::Vector(Vector&& move_vector){
+
+    vector = move_vector.vector;
+    _size = move_vector._size;
+    _capacity = move_vector._capacity;
+    move_vector.vector = nullptr;
+    move_vector._size = 0;
+    move_vector._capacity = 0;
+}
+
+template<typename T> Vector<T>::~Vector(){
+    
+    if(vector != nullptr){
+            
+        delete[] vector; 
+        vector = nullptr;
     }
 }
 
@@ -31,41 +47,59 @@ template<typename T> T& Vector<T>::operator[](size_t position){
 
 template<typename T> const T& Vector<T>::operator[](size_t position) const{
 
-    return *(vector + position)
+    return *(vector + position);
 }
 
 template<typename T> Vector<T>& Vector<T>::operator=(const Vector& right){
 
-    if(right.size() > _capacity){
+    if(this != &right){
+
+        if(right.size() > _capacity) {
+
+            delete[] vector;
+            vector = new T[right.size()];
+            _capacity = right.size();
+        }
+
+        _size = right.size();
+
+        unsigned i = 0;
+        for(;i < right._size; i++)
+            *(vector + i) = right.vector[i];
+
+    }
+
+    return *this;
+    
+}
+
+template<typename T> Vector<T>& Vector<T>::operator=(Vector&& right){
+
+    if(this != &right){
 
         delete[] vector;
-        vector = new T[right.size()];
-        _capacity = right.size();
+         vector = right.vector;
+        _size = right._size;
+        _capacity = right._capacity;
+        right.vector = nullptr;
+        right._size = 0;
+        right._capacity = 0;
     }
 
-    _size = right.size();
-
-    unsigned i = 0;
-    for(auto el : right){
-        
-        *(vector + i) = el;
-        i++;
-    }
+    return *this;
 }
 
 template<typename T> void Vector<T>::expand(size_t new_capacity){
 
-    Vector temp(*this);
+    T* temp = new T[new_capacity];
+
+    unsigned i = 0;
+    for(; i < _size; i++)
+        *(temp + i) = *(vector + i);
+
     delete[] vector;
-    vector = new T[new_capacity];
+    vector = temp;
     _capacity = new_capacity;
-
-    unsigned i  = 0;
-    for(auto el : temp){
-
-        *(vector + i) = el;
-        i++;
-    }
 }
 
 template<typename T> unsigned  Vector<T>::size() const{
@@ -110,12 +144,12 @@ template<typename T> bool Vector<T>::empty() const{
 
 template<typename T> T& Vector<T>::back(){
 
-    return *(vector + _size);
+    return *(vector + _size - 1);
 }
 
 template<typename T> const T& Vector<T>::back() const{
 
-    return *(vector + _size);
+    return *(vector + _size - 1);
 }
 
 template<typename T> void Vector<T>::clear(){
@@ -133,6 +167,22 @@ template<typename T> typename Vector<T>::Iterator Vector<T>::end(){
     return Iterator(vector + _size);
 }
 
+template<typename T> void Vector<T>::display(){
+
+
+    if(empty()){
+
+        cout << "Empty vector"<< endl;
+        return;
+    }
+
+    unsigned i = 0;
+    for(; i < _size; i++)
+        cout << *(vector + i) << " ";
+    
+    cout << endl;
+}
+
 int main() {
 
     Vector<int> v;
@@ -140,16 +190,13 @@ int main() {
     v.push_back(20);
     v.push_back(13);
 
-    Vector<int>::Iterator it = v.begin();
-    for(; it != v.end(); it++)
-        cout<< *it <<endl;
-
-    v.pop_back();
-
-    cout << "After ="<<endl;
+    v.display();
 
     Vector<int> v1 = v;
-
-    for(it = v1.begin(); it != v1.end(); it++)
-        cout<< *it <<endl;
+    v1.display();
+    Vector<int> v2 (move(v));
+    v2.back() = 18;
+    v2[1] = 22;
+    v2.display();
+    v.display();
 }
